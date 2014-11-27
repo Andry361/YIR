7b68f1e0b1e5f73f9ff2efea65c4775a17f07dba
Andry361 <Hsmel73@mail.ru>
2014-11-27 22:17:49 +0200

    First commit

---
+++ УИР/Whush Solution/FFT.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whush.Demo
+{
+  class FFT
+  {
+    private const double pi = 3.14159265358979323846;
+    private int ii;
+    private int jj;
+    private int n;
+    private int mmax;
+    private int m;
+    private int j;
+    private int istep;
+    private int i;
+    private int isign;
+    private double wtemp;
+    private double wr;
+    private double wpr;
+    private double wpi;
+    private double wi;
+    private double theta;
+    private double tempr;
+    private double tempi;
+
+    public FFT()
+    {
+    }
+
+    public void BildFFT(double[] a, int nn, bool inversefft) 
+    {
+      isign = inversefft ? -1 : 1;
+      n = 2 * nn;
+      j = 1;
+      for (ii = 1; ii <= nn; ii++)
+      {
+        i = 2 * ii - 1;
+        if (j > i)
+        {
+          tempr = a[j - 1];
+          tempi = a[j];
+          a[j - 1] = a[i - 1];
+          a[j] = a[i];
+          a[i - 1] = tempr;
+          a[i] = tempi;
+        }
+        m = n / 2;
+        while (m >= 2 && j > m)
+        {
+          j = j - m;
+          m = m / 2;
+        }
+        j = j + m;
+      }
+      mmax = 2;
+      while (n > mmax)
+      {
+        istep = 2 * mmax;
+        theta = 2.0 * pi / (isign * mmax);
+        wpr = -2.0 * Math.Pow(Math.Sin(0.5 * theta), 2);
+        wpi = Math.Sin(theta);
+        wr = 1.0;
+        wi = 0.0;
+        for (ii = 1; ii <= mmax / 2; ii++)
+        {
+          m = 2 * ii - 1;
+          for (jj = 0; jj <= (n - m) / istep; jj++)
+          {
+            i = m + jj * istep;
+            j = i + mmax;
+            tempr = wr * a[j - 1] - wi * a[j];
+            tempi = wr * a[j] + wi * a[j - 1];
+            a[j - 1] = a[i - 1] - tempr;
+            a[j] = a[i] - tempi;
+            a[i - 1] = a[i - 1] + tempr;
+            a[i] = a[i] + tempi;
+          }
+          wtemp = wr;
+          wr = wr * wpr - wi * wpi + wr;
+          wi = wi * wpr + wtemp * wpi + wi;
+        }
+        mmax = istep;
+      }
+      if (inversefft)
+      {
+        for (i = 1; i <= 2 * nn; i++)
+        {
+          a[i - 1] = a[i - 1] / nn;
+        }
+      }
+      return;
+      ///////
+    }
+  }
+}
---
+++ УИР/Whush Solution/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using Microsoft.Research.DynamicDataDisplay;
+using System.Linq;
+using Microsoft.Research.DynamicDataDisplay.DataSources;
+
+
+namespace Whush.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+          InitializeComponent();
+            //var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
+
+            //// Compute y array as sin(x)/x function defined on x grid
+            //var y = x.Select(v => Math.Abs(v) < 1e-10 ? 1 : Math.Sin(v) / v).ToArray();
+            //var xDataSource = x.AsXDataSource();
+            //var yDataSource = y.AsYDataSource();
+            //CompositeDataSource compositeDataSource = xDataSource.Join(yDataSource);
+            //plotter.AddLineGraph(compositeDataSource, Colors.Goldenrod, 3,"Sine");
+            //plotter.FitToView();
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          StBar.Visibility = Visibility.Collapsed;
+          ButtonArrow.Visibility = Visibility.Collapsed;
+          MainGrid.Visibility = Visibility.Collapsed;
+          MenuPagesStacPanel.Width = Double.NaN;
+          MenuItemButtonArrow.Visibility = Visibility.Visible;
+          DoubleAnimation buttonWidth = new DoubleAnimation();
+          buttonWidth.From = Menu.ActualWidth;
+          buttonWidth.To = 160;
+          buttonWidth.Duration = TimeSpan.FromSeconds(0.1);
+          Menu.BeginAnimation(StackPanel.WidthProperty, buttonWidth);          
+        }
+
+        private void MenuItemButtonArrow_Click(object sender, RoutedEventArgs e)
+        {
+          
+          
+          DoubleAnimation buttonWidth = new DoubleAnimation();
+          buttonWidth.From = Menu.ActualWidth;
+          buttonWidth.To = 1;
+          buttonWidth.Duration = TimeSpan.FromSeconds(0.1);
+          Menu.BeginAnimation(StackPanel.WidthProperty, buttonWidth);
+          MainGrid.Visibility = Visibility.Visible;
+          MenuPagesStacPanel.Width = 0;
+          StBar.Visibility = Visibility.Visible;
+          ButtonArrow.Visibility = Visibility.Visible;
+          MenuItemButtonArrow.Visibility = Visibility.Hidden;
+        }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+          Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+          dlg.DefaultExt = ".txt";
+          dlg.Filter = "wav файлы (*.wav)|*.wav";
+
+          Nullable<bool> result = dlg.ShowDialog();
+          if (result == true)
+          {
+            ReadWav file = new ReadWav();
+            byte[] data = file.StartReadWav(dlg.FileName);
+            double step = 1.0;
+
+
+            var x = Enumerable.Range(0, data.Length).Select(i => i * step).ToArray();
+            var y = x.Select(v => data[(int)(v/step)]*1.0).ToArray();
+            
+            var xDataSource = x.AsXDataSource();
+            var yDataSource = y.AsYDataSource();
+            CompositeDataSource compositeDataSource = xDataSource.Join(yDataSource);
+            plotter.AddLineGraph(compositeDataSource, Colors.Goldenrod, 3, "Sine");
+            plotter.FitToView();
+          }
+        }
+    }
+}
